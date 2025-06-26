d356b0ca664b53d6560672408eb36070c1f767d7
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 18:24:07 -0300

    viewset

--- inventarioapp/apps/completedquests/views.py
+++ inventarioapp/apps/completedquests/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import CompletedQuest
 from rest_framework import viewsets
 from .serializer import CompletedQuestSerializer
+
+class CompletedQuestViewSet(viewsets.ModelViewSet):
+    queryset = CompletedQuest.objects.all()
+    serializer_class = CompletedQuestSerializer
