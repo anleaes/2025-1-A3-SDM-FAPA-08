b952bdae46da6f8dfd60666ac6f4f7a2ebe2f273
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 12:28:50 -0300

    verbose apps

--- inventarioapp/apps/playerprofiles/apps.py
+++ inventarioapp/apps/playerprofiles/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PlayerprofilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'playerprofiles'
+    verbose_name = 'Perfis de Jogadores'
