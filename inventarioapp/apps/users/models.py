0ba5c775f6a586d26fd08ed570a43a5956d72358
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 09:40:20 -0300

    criando o models

--- inventarioapp/apps/users/models.py
+++ inventarioapp/apps/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    username = models.CharField('Nome de Usuario', max_length=150, unique=True)
+    email = models.EmailField('Email', unique=True)
+    birth_date = models.DateField('Data de Nascimento', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.username
