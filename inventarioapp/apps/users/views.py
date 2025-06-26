72aa1d473dbb7573893d217c8d88cac93a88d3e8
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 11:01:37 -0300

    criando views

--- inventarioapp/apps/users/views.py
+++ inventarioapp/apps/users/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import User
 from rest_framework import viewsets
 from .serializer import UserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
