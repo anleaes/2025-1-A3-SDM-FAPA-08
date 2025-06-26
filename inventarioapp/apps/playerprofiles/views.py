b9d08f484ad68bfd2956fe5d95de06e954f72a35
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 18:53:52 -0300

    filtro

--- inventarioapp/apps/playerprofiles/views.py
+++ inventarioapp/apps/playerprofiles/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import PlayerProfile
 from rest_framework import viewsets
 from .serializer import PlayerProfileSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PlayerProfileViewSet(viewsets.ModelViewSet):
     queryset = PlayerProfile.objects.all()
     serializer_class = PlayerProfileSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['playername', 'level', 'experience']
