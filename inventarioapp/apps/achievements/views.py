9f89187ab8ef60887c072888d69a94ea8b07632f
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 18:35:49 -0300

    viewset

--- inventarioapp/apps/achievements/views.py
+++ inventarioapp/apps/achievements/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import Achievement
 from rest_framework import viewsets
 from .serializer import AchievementSerializer
+
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
