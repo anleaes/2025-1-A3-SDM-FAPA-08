49ed89a4411ef592f2d42a8dab61b5c62d22b1ca
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 12:57:29 -0300

    att path

--- inventarioapp/inventarioapp/urls.py
+++ inventarioapp/inventarioapp/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('playerprofiles/', include('playerprofiles.urls', namespace='playerprofiles')),
 ]
