ebf09b46b3dd199d2771f96dba3bc8a34135b839
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 17:30:50 -0300

    viewset

--- inventarioapp/apps/quests/views.py
+++ inventarioapp/apps/quests/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import Quest
 from rest_framework import viewsets
 from .serializer import QuestSerializer
+
+class QuestViewSet(viewsets.ModelViewSet):
+    queryset = Quest.objects.all()
+    serializer_class = QuestSerializer
