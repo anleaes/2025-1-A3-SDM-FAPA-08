de69542cc25f53f1575a38274cf6199107f80aa8
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 12:50:43 -0300

    classe PlayerProfileViewSet

--- inventarioapp/apps/playerprofiles/views.py
+++ inventarioapp/apps/playerprofiles/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import PlayerProfile
 from rest_framework import viewsets
 from .serializer import PlayerProfileSerializer
+
+class PlayerProfileViewSet(viewsets.ModelViewSet):
+    queryset = PlayerProfile.objects.all()
+    serializer_class = PlayerProfileSerializer
