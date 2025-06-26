935ac4dfca51840b1724a05353c8f09415903a10
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 18:20:56 -0300

    serializer

--- inventarioapp/apps/completedquests/serializer.py
+++ inventarioapp/apps/completedquests/serializer.py
@@ -1,2 +1,7 @@
 from .models import CompletedQuest
 from rest_framework import serializers
+
+class CompletedQuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompletedQuest
+        fields = '__all__'
