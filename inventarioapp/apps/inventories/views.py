5ec4e8af8a77ec566c113af0e4c7ac6357115c44
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 15:11:31 -0300

    viewset

--- inventarioapp/apps/inventories/views.py
+++ inventarioapp/apps/inventories/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from .models import Inventario
 from rest_framework import viewsets
 from .serializer import InventarioSerializer
+
+class InventarioViewSet(viewsets.ModelViewSet):
+    queryset = Inventario.objects.all()
+    serializer_class = InventarioSerializer
