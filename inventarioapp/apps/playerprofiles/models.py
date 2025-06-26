7de77518896f8ea7112e1fc1e4e8e3e478c99aaf
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 12:27:27 -0300

    add meta e str

--- inventarioapp/apps/playerprofiles/models.py
+++ inventarioapp/apps/playerprofiles/models.py
@@ -10,4 +10,11 @@ class PlayerProfile(models.Model):
     level = models.IntegerField('Nivel', default=1)
     experience = models.IntegerField('Experiencia', default=0)
     total_score = models.IntegerField('Pontuacao Total', default=0)
+
+    class Meta:
+        verbose_name = 'Perfil de Jogador'
+        verbose_name_plural = 'Perfis de Jogadores'
+        ordering = ['id']
     
+    def __str__(self):
+        return f"{self.user.username} - Level {self.level}"
