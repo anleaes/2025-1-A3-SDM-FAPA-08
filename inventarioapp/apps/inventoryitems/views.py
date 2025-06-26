49dfed403347e400f983f4ea888745e3ac96dae4
Miguel md <marquesduartemiguel@gmail.com>
2025-06-26 17:00:21 -0300

    imports

--- inventarioapp/apps/inventoryitems/views.py
+++ inventarioapp/apps/inventoryitems/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import InventoryItem
+from rest_framework import viewsets
+from .serializer import InventoryItemSerializer
